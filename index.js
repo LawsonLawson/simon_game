2fb5f39a0adcfef6a6884087598629e72598454e
LawsonLawson <isreallawson@outlook.com>
2025-10-24 16:49:52 +0000

    fixed allowed clicks after correct sequence bug for better flow

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ var listenForClicks = false;
 
 
 // Detect keypress to start game
-$(document).on("keydown", function () {
-    if (!started) {
+var deviceIsMobile = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+var eventToStart = deviceIsMobile ? "touchstart" : "keydown"
+
+// Should screen size be that of mobile, change prompt
+if (deviceIsMobile) {
+  $("#level-title").text("Tap anywhere apart from the buttons to begin");
+  $("#footer-text").text("Simon Game Mobile Version 7.1 Made By Israel ©");
+} else {
+  $("#level-title").text("Press any key to begin");
+  $("#footer-text").text("Simon Game Desktop Version 7.1 -- Made By Israel ©");
+}
+$(document).on(eventToStart, function () {
+    if (!started && !$(event.target).hasClass("btn")) {
         $("#level-title").text("Level " + level);
         listenForClicks = true;
         started = true;
@@ -34,6 +45,7 @@ $(".btn").on("click", function () {
 
 // Generate next color in the sequence
 function nextSequence() {
+    listenForClicks = false; // don't take clicks after initial sequence is correct
     userClickedPattern = [];
     level++;
     score = level - 1 // increase score with each level passed
@@ -45,6 +57,7 @@ function nextSequence() {
 
     $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
+    listenForClicks = true; // re-enable clicks after next sequence shows
 }
 
 
@@ -53,6 +66,7 @@ function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
         // Check is user is correct so far
         if (userClickedPattern.length === gamePattern.length) {
+            listenForClicks = false; // disable clicks again while the program checks answer
             setTimeout(function () {
                 nextSequence();
             }, 1000);
@@ -61,11 +75,14 @@ function checkAnswer(currentLevel) {
         playSound("wrong");
 
         $("body").addClass("game-over");
+        
         setTimeout(function () {
             $("body").removeClass("game-over");
         }, 200);
 
-        $("#level-title").text("Game Over 😢 | Your Score: " + score + " | Press Any Key to Restart");
+        const gameOverText = "Game Over 😢 | Score: " + score + " | " +
+        (deviceIsMobile ? "Tap anywhere to Restart" : "Press any key to Restart");
+        $("#level-title").text(gameOverText);
 
         listenForClicks = false;
 
@@ -97,4 +114,5 @@ function startOver() {
     gamePattern = [];
     started = false;
     score = 0;
+    listenForClicks = false;
 }
