4e70f67b00af101e932807f47d7bc4ce7ca15e48
LawsonLawson <isreallawson@outlook.com>
2025-10-24 15:00:41 +0000

    refactoring, step 7 to 10 done

--- index.js
+++ index.js
@@ -1,66 +1,100 @@
-// user clicked pattern
-userClickedPattern = [];
+// Track game state
+var gamePattern = [];
+var userClickedPattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var started = false;
+var level = 0;
+var score = 0;
+var listenForClicks = false;
 
-// game pattern
-gamePattern = [];
 
-// array of colors
-buttonColors = ["red", "green", "green", "yellow"];
+// Detect keypress to start game
+$(document).on("keydown", function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        listenForClicks = true;
+        started = true;
+        nextSequence();
+    }
+});
 
 
-/**
- * @function: nextSequence
- * @returns random number between 0 to 3.
- */
+// Detect button click
+$(".btn").on("click", function () {
+    if (!listenForClicks) return;
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+
+// Generate next color in the sequence
 function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    score = level - 1 // increase score with each level passed
+    $("#level-title").text("Level " + level + " | Score: " + score);
+
     var randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
 }
 
-// select a random color
-randomChosenColor = buttonColors[nextSequence()];
 
-// add the random chosen color to the game pattern array to keep track
-gamePattern.push(randomChosenColor);
+// Check user's answer
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        // Check is user is correct so far
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
 
-// selecting the button with the same id as the chosen color so the user can know what the correct button to click is
-$("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
 
-// play sound for the respective button when the user clicks the correct button
-$("#" + randomChosenColor).on("click", function() {
-    playSound(randomChosenColor);
-});
+        $("#level-title").text("Game Over 😢 | Your Score: " + score + " | Press Any Key to Restart");
 
-// listen for the user's click and add that click to the user click pattern array
-$(".btn").on("click", function() {
-    var userChosenColor = this.id;
-    userClickedPattern.push(userChosenColor);
-    playSound(userChosenColor);
-        animatePress(userChosenColor);
-});
+        listenForClicks = false;
+
+        startOver();
+
+    }
+}
 
 
-/**
- * @function: playSound
- * @param fileName: name of sound file
- * @returns a corresponding sound to the button.
- */
-function playSound(fileName) {
-    var audio = new Audio("./sounds/" + fileName + ".mp3");
+// Play corresponding sound
+function playSound(name) {
+    const audio = new Audio("./sounds/" + name + ".mp3");
     audio.play();
 }
 
 
-/**
- * 
- * @function: animatePress
- * @param currentColor: the current color being pressed
- * @returns: animated button.
- */
+// Add animation effect when pressed
 function animatePress(currentColor) {
     $("#" + currentColor).addClass("pressed");
-    
-    setTimeout(function() {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+
+// Restart game variables
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+    score = 0;
 }
